84639be136d9dde9ca4874fedccb6178593c7034
test <test@test.com>
2024-07-31 14:02:40 +0300

    i modified in js v2

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,6 @@ var minus=document.querySelectorAll("#minus");
 
 answers[0].style.display="flex";
 minus[0].style.display="flex";
-questions[0].style.color="#AD28EB";
 
 for(let i=0;i<questions.length && i< answers.length;i++)
 {
@@ -14,17 +13,14 @@ for(let i=0;i<questions.length && i< answers.length;i++)
         {
             answers[i].style.display="none";
             minus[i].style.display="none";
-            questions[i].style.color="hsl(292, 42%, 14%)";
         }
         else{
             for(let j=0 ;j< answers.length;j++){
                 answers[j].style.display="none";
                 minus[j].style.display="none";
-                questions[j].style.color="hsl(292, 42%, 14%)";
             }
             answers[i].style.display="flex";
             minus[i].style.display="flex";
-            questions[i].style.color="#AD28EB";
         }
     });
 }
