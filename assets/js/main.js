cfeed583e8cb20377d5defd20bc6a066b8ad4be8
test <test@test.com>
2024-07-31 12:28:53 +0300

    secont submit edit css and js

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,18 +6,25 @@ var minus=document.querySelectorAll("#minus");
 for(let i=0;i<questions.length && i< answers.length;i++)
 {
     questions[i].addEventListener("click",function(e){
+
+
+
+
         if(answers[i].style.display=="flex")
         {
             answers[i].style.display="none";
             minus[i].style.display="none";
+            questions[i].style.color="hsl(292, 42%, 14%)";
         }
         else{
             for(let j=0 ;j< answers.length;j++){
                 answers[j].style.display="none";
                 minus[j].style.display="none";
+                questions[j].style.color="hsl(292, 42%, 14%)";
             }
             answers[i].style.display="flex";
             minus[i].style.display="flex";
+            questions[i].style.color="#AD28EB";
         }
     });
 }
