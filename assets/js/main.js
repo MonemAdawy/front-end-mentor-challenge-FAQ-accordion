4b26be6f44a9ff1723be909bd141c4f28b2b1285
test <test@test.com>
2024-07-31 12:11:39 +0300

    first supmit

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+var questions=document.getElementsByClassName("p-question");
+var answers=document.querySelectorAll("#p-answer");
+var plus=document.querySelectorAll("#plus");
+var minus=document.querySelectorAll("#minus");
+
+for(let i=0;i<questions.length && i< answers.length;i++)
+{
+    questions[i].addEventListener("click",function(e){
+        if(answers[i].style.display=="flex")
+        {
+            answers[i].style.display="none";
+            minus[i].style.display="none";
+        }
+        else{
+            for(let j=0 ;j< answers.length;j++){
+                answers[j].style.display="none";
+                minus[j].style.display="none";
+            }
+            answers[i].style.display="flex";
+            minus[i].style.display="flex";
+        }
+    });
+}
